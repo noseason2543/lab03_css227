0f73fcd62acfceee076ffd8b3ed0237cca31a757
thanadon lamsan <thanadon11406@gmail.com>
2021-02-10 21:12:52 +0700

    my first save lab03 css227

---
+++ lab03_01_408.js
@@ -0,0 +1,4 @@
+var ch = prompt("What is your favorite charactor?");
+var clr = prompt("What is the main color of "+ch);
+console.log(ch+" is your favorite charactor.");
+console.log(ch +" is "+clr);
---
+++ lab03_02_408.js
@@ -0,0 +1,177 @@
+function omg(){
+    if(document.getElementById('t1').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t2').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t3').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t1').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t2').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t3').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t1').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t4').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t7').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t1').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t4').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t7').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t1').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t9').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t1').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t9').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t2').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t8').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t2').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t8').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t4').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t6').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t4').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t6').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t3').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t7').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t3').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t7').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t3').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t6').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t9').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t3').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t6').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t9').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t7').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t8').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t9').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t7').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t8').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t9').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }
+    else if(this.src =='https://static-marketing-2.onestockhome.com/products/43186409/images/20801_m.jpg'){
+        this.src = 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png';
+        var check=[];
+        while(true){
+            var a1 = Math.floor(Math.random()*9+1);
+            var a2 = 't'+a1;
+            
+            if(document.getElementById(a2).src !='https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png' 
+            && document.getElementById(a2).src !='https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+                document.getElementById(a2).src = 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png';
+                break;
+            }else{
+                if(!check.includes(a2) && check.length <=9){
+                    check.push(a2);
+                    console.log(check);
+                }else if(check.length < 9){
+                    continue;
+                }else{
+                    break;
+                }
+            }
+        }
+        if(document.getElementById('t1').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t2').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t3').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t1').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t2').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t3').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t1').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t4').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t7').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t1').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t4').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t7').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t1').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t9').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t1').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t9').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t2').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t8').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t2').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t8').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t4').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t6').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t4').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t6').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t3').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t5').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t7').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t3').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t5').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t7').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t3').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t6').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t9').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t3').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t6').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t9').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }else if(document.getElementById('t7').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t8').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'
+        && document.getElementById('t9').src == 'https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png'){
+            document.getElementById('result').innerText =" X win";
+        }else if(document.getElementById('t7').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t8').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'
+        && document.getElementById('t9').src == 'https://www.pngkey.com/png/detail/205-2056274_tic-tac-toe-img-letter-o.png'){
+            document.getElementById('result').innerText =" O win";
+        }
+    }else{
+        alert("try again.");
+    }
+    
+    
+}
+var c1 = document.getElementById('t1');
+c1.addEventListener('click',omg);
+var c2 = document.getElementById('t2');
+c2.addEventListener('click',omg);
+var c3 = document.getElementById('t3');
+c3.addEventListener('click',omg);
+var c4 = document.getElementById('t4');
+c4.addEventListener('click',omg);
+var c5 = document.getElementById('t5');
+c5.addEventListener('click',omg);
+var c6 = document.getElementById('t6');
+c6.addEventListener('click',omg);
+var c7 = document.getElementById('t7');
+c7.addEventListener('click',omg);
+var c8 = document.getElementById('t8');
+c8.addEventListener('click',omg);
+var c9 = document.getElementById('t9');
+c9.addEventListener('click',omg);
